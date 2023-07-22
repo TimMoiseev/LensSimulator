22b826c3002976817174fab34b3bf2ae118c0a35
TimMoiseev <lglglg112@gmail.com>
2023-07-22 15:28:02 +0700

    MVVM начало

---
+++ Model/Graphics/GraphicsEngine.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LensSimulator.Model.Graphics
+{
+    internal class GraphicsEngine
+    {
+        public void runEngine()
+        {
+            state.IsRunning = true;
+            state.IsError = false;
+        }
+        public void stopEngne()
+        {
+            state.IsRunning = false;
+        }
+        private EngineState state;
+        public EngineState State { get { return state; } set { } }
+        
+    }
+    struct EngineState : INotifyPropertyChanged
+    {
+        bool isError;
+        bool isRunning;
+        public bool IsRunning
+        {
+            get { return isRunning; }
+            set 
+            { 
+                isRunning = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool IsError
+        {
+            get { return isError; }
+            set 
+            { 
+                isError = value;
+                OnPropertyChanged();
+            }
+        }
+        public EngineState()
+        {
+            isRunning = false;
+            isError = false;
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null && !IsError)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using LensSimulator.Model.Graphics;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LensSimulator.ViewModel
+{
+    internal class MainViewModel : INotifyPropertyChanged
+    {
+        private GraphicsEngine engine;
+
+        public string EngineState
+        {
+            get
+            {
+                if(!engine.State.IsError && engine.State.IsRunning) { return "GE Running \n"; }
+                else { return "GE Stopped \n"; }
+            }
+        }
+        public MainViewModel() 
+        {
+            (engine = new()).runEngine();
+            Thread.Sleep(10000);
+            engine.stopEngne();
+
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
