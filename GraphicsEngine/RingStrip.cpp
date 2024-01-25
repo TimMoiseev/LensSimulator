9525a0df384397ff160cc8fcfe084b32ebcc8a7e
TimMoiseev <lglglg112@gmail.com>
2024-01-25 14:43:27 +0700

    Исправил ошибку освещения
    Исправил ошибку связанную с расчетом нормалей в сущности RingStrip

--- GraphicsEngine/RingStrip.cpp
+++ GraphicsEngine/RingStrip.cpp
@@ -7,7 +7,14 @@ void RingStrip::createModel()
 	vec3 zAxis{0.0f, 0.0f, 1.0f};
 	vec3 rotAxis = glm::cross(zAxis, o);
 	float angle = glm::angle(zAxis, o);
-	quat rot = glm::angleAxis(angle, glm::normalize(rotAxis));
+	quat rot{};
+	if (rotAxis != vec3{ 0.0,0.0,0.0 }) {
+		rot = glm::angleAxis(angle, glm::normalize(rotAxis));
+	}
+	else {
+		rot = quat{ 0.0,0.0,0.0,0.0 };
+	}
+	
 	for (int i = -n; i < n; ++i) {
 		vec4 ringDot = 
 			rot * vec4(vec3(0.5 * d * glm::cos(2.0f * glm::pi<float>() * i / n) , 0.5 * d * glm::sin(2.0f * glm::pi<float>() * i / n), h / 2.0f), 0.0); //quat*dot*conjugate_quat - так нельзя, в glm поворот делается так quat*dot
@@ -15,9 +22,9 @@ void RingStrip::createModel()
 			rot * vec4(vec3(0.5 * d * glm::cos(2.0f * glm::pi<float>() * i / n), 0.5 * d * glm::sin(2.0f * glm::pi<float>() * i / n), -h / 2.0f), 0.0);
 		vec3 hOffset = o * h / 2.0f;
 		vertices->push_back(
-			Vertex{ { ringDot.x + p.x, ringDot.y + p.y, ringDot.z + p.z }, {1.0, 1.0, 1.0}, {ringDot.x + hOffset.x, ringDot.y + hOffset.y, ringDot.z + hOffset.z} });
+			Vertex{ { ringDot.x + p.x, ringDot.y + p.y, ringDot.z + p.z }, {1.0, 1.0, 1.0}, {ringDot.x - hOffset.x, ringDot.y - hOffset.y, ringDot.z - hOffset.z} });
 		vertices->push_back(
-			Vertex{ {oppositeRingDot.x + p.x, oppositeRingDot.y + p.y, oppositeRingDot.z + p.z}, {1.0, 1.0, 1.0}, {oppositeRingDot.x - hOffset.x, oppositeRingDot.y - hOffset.y, oppositeRingDot.z - hOffset.z} });
+			Vertex{ {oppositeRingDot.x + p.x, oppositeRingDot.y + p.y, oppositeRingDot.z + p.z}, {1.0, 1.0, 1.0}, {oppositeRingDot.x + hOffset.x, oppositeRingDot.y + hOffset.y, oppositeRingDot.z + hOffset.z} });
 	}
 	for (int i = 0; i < 2*n; ++i) {
 		indices->push_back(i);
